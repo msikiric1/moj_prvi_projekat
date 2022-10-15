288f32691eb535ed41a8b5222c2e3da3504a7e1f
msikiric1 <msikiric1@etf.unsa.ba>
2022-10-15 13:05:03 +0200

    Promijenjen tekst koji se ispisuje na ekran

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("neka promjena");
+        System.out.println("Vozdra raja");
     }
 }
